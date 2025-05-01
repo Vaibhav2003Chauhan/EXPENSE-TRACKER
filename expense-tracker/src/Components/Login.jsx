bd40eb1ad07791a8579c9d69dbba1ef6c42c7180
Vaibhav Chauhan <vishuchauhan461@gmail.com>
2025-05-01 23:38:30 +0530

    Added the LOGIN component in the Frontend

---
+++ expense-tracker/src/Components/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useState } from 'react';
+
+function Login({ setuser }) {
+    const [username, setusename] = useState(null);
+    const [password, setpassword] = useState(null);
+
+    return (
+        <>
+            <div className="login_main_container ">
+
+            </div>
+        </>
+    )
+}
+
+export default Login
